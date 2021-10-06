eaf4c6bbd33306c2ff5cd2bf521c03ee16fb1f9f
Eliut González <reliutg@gmail.com>
2021-10-06 19:45:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,250 @@
+<template>
+  <nav>
+    <h1>BuzzNotify</h1>
+    <ul>
+      <li>
+        <a href="https://github.com/eliutgon/buzz-notify" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+            ></path>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/eliutgon" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+            ></path>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <button id="dark-mode">
+          <img src="./icons/moon.svg" alt="Dark mode toggle icon" />
+        </button>
+      </li>
+    </ul>
+  </nav>
+
+  <section class="buttons">
+    <h1>Top</h1>
+    <button
+      class="btn btn--success"
+      data-position="top left"
+      data-type="success"
+      @click="createNotification"
+    >
+    
+      Success
+    </button>
+    <button
+      class="btn btn--danger"
+      data-position="top center"
+      data-type="danger"
+      @click="createNotification"
+    >
+    
+      Danger
+    </button>
+    <button
+      class="btn btn--warning"
+      data-position="top right"
+      data-type="warning"
+      @click="createNotification"
+    >
+    
+      Warning
+    </button>
+    <h1>Bottom</h1>
+    <button
+      class="btn btn--success"
+      data-position="bottom left"
+      data-type="success"
+      @click="createNotification"
+    >
+    
+      Success
+    </button>
+    <button
+      class="btn btn--danger"
+      data-position="bottom center"
+      data-type="danger"
+      @click="createNotification"
+    >
+    
+      Danger
+    </button>
+    <button
+      class="btn btn--warning"
+      data-position="bottom right"
+      data-type="warning"
+      @click="createNotification"
+      data-duration="-18"
+    >
+      Warning
+    </button>
+  </section>
+  
+  <div id="notify"></div>
+</template>
+
+<script>
+import Notify from "@reliutg/buzz-notify";
+import "@reliutg/buzz-notify/dist/buzz-notify.css";
+
+export default {
+  name: "App",
+  data(){
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    createNotification({ currentTarget }) {
+      const options = Object.assign({}, currentTarget.dataset);
+      Notify({ ...options, title: `Notification ${this.count}` });
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Roboto:400,400i,700");
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: Roboto, sans-serif;
+}
+
+nav,
+main {
+  width: 80%;
+  margin: auto;
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+  text-align: center;
+}
+
+nav ul {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+@media (min-width: 700px) {
+  nav {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  nav ul {
+    width: 10%;
+  }
+}
+
+nav ul li {
+  display: inline-block;
+  color: #202124;
+}
+
+nav ul li a {
+  color: #202124;
+}
+
+nav ul #dark-mode {
+  padding: 0;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+.dark {
+  background-color: #202124;
+  color: #fff;
+}
+
+.dark nav ul li a {
+  color: #fff;
+}
+
+.buttons {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.btn {
+  color: hsl(0 0% 96%);
+  font-size: 20px;
+  margin: 14px;
+  font-family: inherit;
+  padding: 14px 28px;
+  border: 0;
+  border-radius: 6px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 0.15s ease-in-out;
+  box-shadow: 0 4px 6px -1px hsl(0 0% 0% / 0.07),
+    0 2px 4px -1px hsl(0 0% 0% / 0.07);
+}
+
+.btn:active {
+  transform: scale(0.96);
+}
+
+.btn--success {
+  color: #155724;
+  background-color: #d4edda;
+  border-color: #c3e6cb;
+}
+
+.btn--danger {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+}
+.btn--warning {
+  color: #856404;
+  background-color: #fff3cd;
+  border-color: #ffeeba;
+}
+</style>
