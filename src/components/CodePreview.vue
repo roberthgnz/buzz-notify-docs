a7b78bf9490b078fab01ded19fee4d3b29046ecf
Eliut González <reliutg@gmail.com>
2021-10-07 18:04:55 +0200

    feat: code highlight

--- src/components/CodePreview.vue
+++ src/components/CodePreview.vue
@@ -1,14 +1,23 @@
 <template>
-  <pre>{{code}}</pre>
+  <code v-html="preview"></code>
 </template>
 
 <script>
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
+
 export default {
   props: {
     code: {
       type: Object,
       required: true,
       default: () => ({}),
+    },
+  },
+  computed: {
+    preview() {
+      const text = `Notify(${JSON.stringify(this.code, null, 2)})`;
+      return hljs.highlight(text, { language: "json" }).value;
     },
   },
 };
