87ac860eeb6fdf3485b87b0351dc303518fa1fab
Sakthivel <coppersakthi@gmail.com>
2023-10-10 09:44:26 +0530

    added a copy code button

--- src/components/CodePreview.vue
+++ src/components/CodePreview.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="code-preview">
     <pre v-html="preview"></pre>
+    <button @click="copyToClipboard" :disabled="isCopied">
+      <span v-if="!isCopied" class="far fa-copy"> Copy code</span>
+      <i v-else>Copied!</i>
+    </button>
   </div>
 </template>
 
@@ -9,6 +13,11 @@ import hljs from "highlight.js";
 import "highlight.js/styles/monokai-sublime.css";
 
 export default {
+  data() {
+    return {
+      isCopied: false,
+    };
+  },
   props: {
     code: {
       type: Object,
@@ -22,6 +31,22 @@ export default {
       return hljs.highlight(text, { language: "json" }).value;
     },
   },
+  methods: {
+    copyToClipboard() {
+      const el = document.createElement("textarea");
+      el.value = `Notify(${JSON.stringify(this.code, null, 2)})`;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand("copy");
+      document.body.removeChild(el);
+      // Update the state to show "Copied"
+      this.isCopied = true;
+      // Reset after a short delay (e.g., 2 seconds)
+      setTimeout(() => {
+        this.isCopied = false;
+      }, 2000);
+    },
+  },
 };
 </script>
 
@@ -31,7 +56,23 @@ export default {
   border-radius: 4px;
   border: 1px solid #eaeaea;
 }
+
 .dark .code-preview {
   border-color: #222;
+}
+
+button {
+  background-color: #4a4a4a; /* Dark background color */
+  color: #ffffff; /* Text color */
+  padding: 6px 12px; /* Adjusted padding for a smaller button */
+  font-size: 14px; /* Adjusted font size */
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease; /* Smooth transition on hover */
+}
+
+button:hover {
+  background-color: #333; /* Darker background color on hover */
 }
 </style>
